2ac3db7c8ab3a19f0b39486742359e3a92bd0784
s1131610 <s1131610@monmouth.edu>
2021-03-23 20:29:18 -0400

    update event works!

--- app/_services/event.service.ts
+++ app/_services/event.service.ts
@@ -1,21 +1,25 @@
-import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { Injectable } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
 
-import { Event } from '../_models';
+import { Event } from "../_models";
 
-@Injectable({ providedIn: 'root' })
+@Injectable({ providedIn: "root" })
 export class EventService {
-    constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {}
 
-    getAll() {
-        return this.http.get<Event[]>(`/events`);
-    }
-   createEvent(event: Event) {
-        console.log("KAK event-service.createEvent - creating event:",event);
-        return this.http.post(`/events/create`, event);
-    }
+  getAll() {
+    return this.http.get<Event[]>(`/events`);
+  }
+  createEvent(event: Event) {
+    console.log("KAK event-service.createEvent - creating event:", event);
+    return this.http.post(`/events/create`, event);
+  }
+  updateEvent(id: string, params) {
+    console.log("KAK event-service.updateEvent - updating event with id:", id);
 
-   /* delete(id: number) {
+    return this.http.post(`/events/update/${id}`, params);
+  }
+  /* delete(id: number) {
         return this.http.delete(`/events/${id}`);
     }
     */
